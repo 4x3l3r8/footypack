87c9b37fa5cb50e7ea896b930c3ff3d7b068bf77
Ibrahim Damilola Dhikirullahi <oloyedammy@gmail.com>
2023-09-04 07:07:06 +0100

    noce

--- src/components/onboarding/SignUp/FormMain.tsx
+++ src/components/onboarding/SignUp/FormMain.tsx
@@ -68,10 +68,7 @@ const FormMain = () => {
             enableReinitialize
             validationSchema={signUpValidation}
             onSubmit={(values, { setSubmitting }) => {
-              // setTimeout(() => {
-              //   alert(JSON.stringify(values, null, 2))
-              //   setSubmitting(false)
-              // }, 400)
+              
               updateUser.mutate({
                 firstname: values.firstName,
                 lastname: values.lastName,
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
-import { type Config } from "tailwindcss";
-
+// import { type Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
